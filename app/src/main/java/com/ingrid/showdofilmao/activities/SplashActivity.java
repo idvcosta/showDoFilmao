fa1bbe09d92515678538245a7a64b67697a31823
Ingrid <ingrid.danielle@gmail.com>
2020-11-22 10:32:43 -0300

    Changed splash to finish it when opening game screen Changed splash to dispose handler

--- app/src/main/java/com/ingrid/showdofilmao/activities/SplashActivity.java
+++ app/src/main/java/com/ingrid/showdofilmao/activities/SplashActivity.java
@@ -11,19 +11,25 @@ import com.ingrid.showdofilmao.R;
 public class SplashActivity extends AppCompatActivity {
 
     private static final long SPLASH_TIME = 5000;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Handler handler = new Handler();
+        handler = new Handler();
         handler.postDelayed(() -> openGameActivity(), SPLASH_TIME);
     }
 
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
 
     private void openGameActivity() {
         startActivity(new Intent(this, GameActivity.class));
+        finish();
     }
 }
